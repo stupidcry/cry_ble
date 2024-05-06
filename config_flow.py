ab60a247209ccb4dd8e8c016600604dd74bca1ff
Your Name <you@example.com>
2024-05-06 13:35:38 +0000

    update

--- config_flow.py
+++ config_flow.py
@@ -77,11 +77,14 @@ class OralBConfigFlow(ConfigFlow, domain=DOMAIN):
 
         current_addresses = self._async_current_ids()
         for discovery_info in async_discovered_service_info(self.hass, False):
+            print(f"===================={discovery_info}")
+            print(f"===================={discovery_info.address}")
             address = discovery_info.address
             if address in current_addresses or address in self._discovered_devices:
                 continue
             device = DeviceData()
-            if device.supported(discovery_info):
+            # if device.supported(discovery_info):
+            if True:
                 self._discovered_devices[address] = (
                     device.title or device.get_device_name() or discovery_info.name
                 )
