83d92bfcb477f3d0921f5f4bd8f13401a2dd36b2
Your Name <you@example.com>
2024-05-08 07:36:46 +0000

    update

--- __init__.py
+++ __init__.py
@@ -108,7 +108,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         connectable=False,
     )
 
-    await coordinator.async_config_entry_first_refresh()
+    # await coordinator.async_config_entry_first_refresh()
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
