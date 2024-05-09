57906f6906dde30dddfaa08c5263414c33a23bc4
Your Name <you@example.com>
2024-05-09 11:11:01 +0000

    update

--- QingBluetoothDeviceData.py
+++ QingBluetoothDeviceData.py
@@ -1,6 +1,47 @@
 from bluetooth_sensor_state_data import BluetoothData
+from home_assistant_bluetooth import BluetoothServiceInfo
+from bleak.backends.device import BLEDevice
+from sensor_state_data import (
+    BinarySensorDeviceClass,
+    SensorLibrary,
+    SensorUpdate,
+    Units,
+)
+from bleak_retry_connector import establish_connection
+from bleak import BleakClient
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class QingBluetoothDeviceData(BluetoothData):
     def __init__(self, bindkey: bytes | None = None) -> None:
         super().__init__()
+
+    def poll_needed(
+        self, service_info: BluetoothServiceInfo, last_poll: float | None
+    ) -> bool:
+        _LOGGER.warning("*** poll_needed")
+        return True
+
+    async def async_poll(self, ble_device: BLEDevice) -> SensorUpdate:
+        """
+        Poll the device to retrieve any values we can't get from passive listening.
+        """
+        _LOGGER.warning("*** async_poll")
+        # if self.device_id == 0x0098:
+        #     client = await establish_connection(
+        #         BleakClient, ble_device, ble_device.address
+        #     )
+        #     try:
+        #         battery_char = client.services.get_characteristic(
+        #             CHARACTERISTIC_BATTERY
+        #         )
+        #         payload = await client.read_gatt_char(battery_char)
+        #     finally:
+        #         await client.disconnect()
+
+        #     self.set_device_sw_version(payload[2:].decode("utf-8"))
+        #     self.update_predefined_sensor(SensorLibrary.BATTERY__PERCENTAGE, payload[0])
+
+        return self._finish_update()
--- __init__.py
+++ __init__.py
@@ -31,7 +31,12 @@ from .const import (
     XIAOMI_BLE_EVENT,
     XiaomiBleEvent,
 )
+from .QingActiveBluetoothProcessorCoordinator import (
+    QingActiveBluetoothProcessorCoordinator,
+)
+
 # from .coordinator import XiaomiActiveBluetoothProcessorCoordinator
+from .QingBluetoothDeviceData import QingBluetoothDeviceData
 
 PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.EVENT, Platform.SENSOR]
 
@@ -40,6 +45,69 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     _LOGGER.warning("*** async_setup_entry:%s", entry)
+    address = entry.unique_id
+    assert address is not None
+
+    data = QingBluetoothDeviceData()
+
+    def _needs_poll(
+        service_info: BluetoothServiceInfoBleak, last_poll: float | None
+    ) -> bool:
+        # Only poll if hass is running, we need to poll,
+        # and we actually have a way to connect to the device
+        return (
+            hass.state is CoreState.running
+            and data.poll_needed(service_info, last_poll)
+            and bool(
+                async_ble_device_from_address(
+                    hass, service_info.device.address, connectable=True
+                )
+            )
+        )
+
+    async def _async_poll(service_info: BluetoothServiceInfoBleak) -> SensorUpdate:
+        # BluetoothServiceInfoBleak is defined in HA, otherwise would just pass it
+        # directly to the Xiaomi code
+        # Make sure the device we have is one that we can connect with
+        # in case its coming from a passive scanner
+        if service_info.connectable:
+            connectable_device = service_info.device
+        elif device := async_ble_device_from_address(
+            hass, service_info.device.address, True
+        ):
+            connectable_device = device
+        else:
+            # We have no bluetooth controller that is in range of
+            # the device to poll it
+            raise RuntimeError(
+                f"No connectable device found for {service_info.device.address}"
+            )
+        return await data.async_poll(connectable_device)
+
+    coordinator = hass.data.setdefault(DOMAIN, {})[entry.entry_id] = (
+        QingActiveBluetoothProcessorCoordinator(
+            hass,
+            _LOGGER,
+            address=address,
+            mode=BluetoothScanningMode.PASSIVE,
+            update_method=data.update,
+            needs_poll_method=_needs_poll,
+            device_data=data,
+            discovered_event_classes=set(
+                entry.data.get(CONF_DISCOVERED_EVENT_CLASSES, [])
+            ),
+            poll_method=_async_poll,
+            # We will take advertisements from non-connectable devices
+            # since we will trade the BLEDevice for a connectable one
+            # if we need to poll it
+            connectable=False,
+            entry=entry,
+        )
+    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    entry.async_on_unload(
+        coordinator.async_start()
+    )  # only start after all platforms have had a chance to subscribe
     return True
 
 
